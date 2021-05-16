333840c56300b0470747c34e2b9465a8c2ef21fd
sibunnguyen2k <78847721+sibunnguyen2k@users.noreply.github.com>
2021-05-16 13:48:34 +0700

    init project framework

---
+++ testcases/com/nopcommerce/user/NewTest.java
@@ -0,0 +1,9 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ testcases/com/nopcommerce/user/User_03_MyAccount.java
@@ -0,0 +1,21 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class User_03_MyAccount {
+	@BeforeClass
+	public void beforeClass() {
+		
+	}
+	@Test
+	public void TC_01_Register_Empty_Data(){
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		
+	}
+}
